9abe32873da279e6944fdb3f79fc54a10d862558
Lichi <litzyargueta7@gmail.com>
2025-11-26 21:59:38 -0600

    zz

--- src/views/perfilView.vue
+++ src/views/perfilView.vue
@@ -3,20 +3,19 @@
     <br>
     <br>
     <perfilComponente></perfilComponente>
-    <br>
-    <footerComponente></footerComponente>
+   
 </template>
 
 <script>
 import perfilComponente from '@/components/perfilComponente.vue'
 import NavComponente from '@/components/NavComponente.vue';
-import footerComponente from '@/components/footerComponente.vue';
+
 
 export default {
     components: {
         perfilComponente,
-        NavComponente,
-        footerComponente
+        NavComponente
+       
     }
 }
 </script>
