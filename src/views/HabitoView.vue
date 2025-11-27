c8ed952278e60cc659ea66c627e3fa7d646ab569
Lichi <litzyargueta7@gmail.com>
2025-11-26 19:45:50 -0600

    confoot

--- src/views/HabitoView.vue
+++ src/views/HabitoView.vue
@@ -4,16 +4,19 @@
   <div>
     <HabitosComponente />
   </div>
+  <footerComponente/>
 </template>
 
 <script>
 import HabitosComponente from '../components/HabitosComponente.vue';
 import NavComponente from '@/components/NavComponente.vue';
+import footerComponente from '@/components/footerComponente.vue';
 export default {
   name: 'HabitosView',
   components: {
     HabitosComponente,
-    NavComponente
+    NavComponente,
+    footerComponente
   }
 }
 </script>
